8c4b53ce5b20a45fe59d997cf286f0ff39efff33
asm1127 <marjona1marjona2@gmail.com>
2025-12-01 17:57:06 +0500

    yechim

---
+++ 1.12.2025.py
@@ -0,0 +1,26 @@
+# son = int(input("Sonni kiriting: "))
+#
+# yigindi = 0
+# asl_son = son
+#
+# while asl_son > 0:
+#     raqam = asl_son % 10
+#     yigindi = yigindi + raqam
+#     asl_son = asl_son // 10
+#
+# print(f"{son} ning raqamlar yigindisi: {yigindi}")
+
+# son = int(input("Sonni kiriting: "))
+#
+# boluvchilar_yigindisi = 0
+# boluvchi = 0
+#
+# while boluvchi < son:
+#     if son % boluvchi == 0:
+#         boluvchilar_yigindisi = boluvchilar_yigindisi + boluvchi
+#     boluvchi = boluvchi + 1
+#
+# if boluvchilar_yigindisi == son:
+#     print(f"{son} - mukammal son")
+# else:
+#     print(f"{son} - mukammal son emas!")
